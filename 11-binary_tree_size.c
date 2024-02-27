efb5e9db3c05d4e13591449a2cb77b970c5ab4fe
Richouf95 <wourichouf95@gmail.com>
2024-02-27 06:01:55 -0800

    Binary tree size

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - the tree size
+ * @tree: the tree
+ * Return: the size
+*/
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -46,6 +46,7 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_height(const binary_tree_t *tree);
 size_t get_max(size_t a, size_t b);
 size_t binary_tree_depth(const binary_tree_t *tree);
+size_t binary_tree_size(const binary_tree_t *tree);
 
 
 #endif /* _BINARY_TREES_H_ */
