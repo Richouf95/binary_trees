b0a7a8b2a7213bf682052f2d3bab90a13a4b21b0
Richouf95 <wourichouf95@gmail.com>
2024-02-27 02:10:50 -0800

    Get a tree deep

--- binary_trees.h
+++ binary_trees.h
@@ -45,6 +45,7 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_height(const binary_tree_t *tree);
 size_t get_max(size_t a, size_t b);
+size_t binary_tree_depth(const binary_tree_t *tree);
 
 
 #endif /* _BINARY_TREES_H_ */
