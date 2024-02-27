3863980880251c5eec40d9e60de1d7a69da583d2
Richouf95 <wourichouf95@gmail.com>
2024-02-27 11:08:27 -0800

    Fix calculate the balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -4,7 +4,7 @@
  * binary_tree_balance - calculate the balance
  * @tree: the tree
  * Return: the balance
-*/
+ */
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
@@ -18,20 +18,20 @@ int binary_tree_balance(const binary_tree_t *tree)
  * binary_tree_height - calculate tree heigh
  * @tree: the tree
  * Return: Tree height
-*/
+ */
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        size_t l, r;
+	size_t l, r;
 
-        if (tree == NULL)
-                return (0);
+	if (tree == NULL)
+		return (0);
 
-        if (tree->left != NULL)
+	if (tree->left != NULL)
 		l = binary_tree_height(tree->left) + 1;
 	else
 		l = 1;
-        
+
 	if (tree->right != NULL)
 		r = binary_tree_height(tree->right) + 1;
 	else
@@ -40,5 +40,5 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (l > r)
 		return (l);
 
-        return (r);
+	return (r);
 }
