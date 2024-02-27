32f56def55c9a59f67ef697c672cc62787307c1e
Richouf95 <wourichouf95@gmail.com>
2024-02-27 14:00:46 -0800

    Find sibling node

--- binary_trees.h
+++ binary_trees.h
@@ -54,5 +54,6 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 int height(const binary_tree_t *tree);
 int is_perfect_util(const binary_tree_t *tree, int height, int level);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 
 #endif /* _BINARY_TREES_H_ */
